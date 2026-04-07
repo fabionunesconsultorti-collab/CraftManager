1cf17e0fa644d6ccfaab1c8dd52e16199ef104d7
Fabio Nunes <fabionunes@github.com>
2026-04-07 16:47:35 -0300

    Config: Habilita acesso HTTP via rede interna (0.0.0.0)

--- app.py
+++ app.py
@@ -488,4 +488,5 @@ def atualizar_fase_pedido(pedido_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+    # Permitindo acesso pela rede local via host '0.0.0.0'
     app.run(host='0.0.0.0', debug=True, port=5000)
